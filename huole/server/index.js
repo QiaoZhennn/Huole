a9f81d4e3e12f3d71c089cd30d23172066afe1dd
Hao Tang <tanghao@huobi.com>
2018-10-19 14:45:54 -0700

    add prod configs
    Test Plan: NA
    
    Reviewers: qiaozhen
    
    Reviewed By: qiaozhen
    
    Subscribers: wangkai1124
    
    Differential Revision: http://phabricator.huobidev.com/D17992

--- huole/server/index.js
+++ huole/server/index.js
@@ -9,21 +9,16 @@ var port = process.env.PORT || 8000;
 
 // Add headers
 app.use(function (req, res, next) {
+  var allowedOrigins = ['http://localhost:3000', 'http://huole.huobidev.com'];
+  var origin = req.headers.origin;
+  if(allowedOrigins.indexOf(origin) > -1){
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  } 
 
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-  // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
 
-  // Pass to next layer of middleware
   next();
 });
 
@@ -32,19 +27,21 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
 import morganBody from 'morgan-body';
 morganBody(app);
-
 
 app.get('/', function(req, res) {
   res.status(200).send({success: 'true', message: 'hooray! welcome to our api!'});   
 });
 
-
 var Web3 = require('web3');
-// var web3 = new Web3(new Web3.providers.HttpProvider('http://172.18.14.10:8501'));
-var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+var web3;
+if (process.env.NODE_ENV == 'prod') {
+  web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8501'));
+} else {
+  web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+}
+
 web3.eth.defaultAccount = process.env.WALLET_ADDRESS;
 const addr = process.env.WALLET_ADDRESS;
 console.log("private", process.env.WALLET_PRIVATE_KEY);
